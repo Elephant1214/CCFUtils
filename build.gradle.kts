00082e45aa21c5d604cd6d1b846f8619d651e88a
Elephant1214 <nathangetsmail@gmail.com>
2023-10-29 15:21:08 -0600

    I guess I did need that

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,3 +27,14 @@ java {
     withSourcesJar()
     withJavadocJar()
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+            from(components["java"])
+        }
+    }
+}
